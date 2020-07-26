a29d0c7b0898bfa951a3791757ee354f417abb37
aaron long legs <longaaron331@gmail.com>
2020-07-26 15:28:48 +0100

    updated version, still one bug

--- src/main/java/com/atm/main/ATM.java
+++ src/main/java/com/atm/main/ATM.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 import org.apache.commons.cli.CommandLine;
 
+import com.atm.common.AtmError;
 import com.atm.datamodel.AtmMachine;
 import com.atm.datamodel.UserAccount;
 
@@ -25,7 +26,7 @@ public class ATM {
 		UserSession details = new UserSession();
 		AtmOverdraft overdraft = new AtmOverdraft();
 		AtmBalance bal = new AtmBalance();
-	    //AtmWithdrawal withdrawal = new AtmWithdrawal();
+		// AtmWithdrawal withdrawal = new AtmWithdrawal();
 		AtmMachine atmMachine = new AtmMachine();
 
 		/*
@@ -44,50 +45,68 @@ public class ATM {
 			String firstLine = atmSessionFile.nextLine();
 
 			String[] firstLineValueArr = firstLine.split(" ");
-            // Identifies the first line of the file as the Atm balance
+			// Identifies the first line of the file as the Atm balance
 			if (firstLineValueArr.length == 1) {
 				int atmBalance = Integer.parseInt(firstLineValueArr[0]); // try bl9ck
 				atmMachine.setBalance(atmBalance);
-                // Identifies the start of the User session
+				// Identifies the start of the User session
 				while (sc.hasNextLine()) {
 					String line = sc.nextLine();
 					String[] userSessionLineArr = line.split(" ");
 
+					// Identifies User account details
 					if (userSessionLineArr.length == 3) {
-						UserAccount userAccount = new UserAccount();
-						userAccount.setAccountNumber(userSessionLineArr[0]);
-						userAccount.setAccountPin(userSessionLineArr[1]);
+						// validation for AccountEnum
+						if (userSessionLineArr[1].equals(userSessionLineArr[2])) {
+							UserAccount userAccount = new UserAccount();
+							userAccount.setAccountNumber(userSessionLineArr[0]);
+							userAccount.setAccountPin(userSessionLineArr[1]);
 
-						line = sc.nextLine();
-						String[] userBalanceArr = line.split(" ");
+							line = sc.nextLine();
+							String[] userBalanceArr = line.split(" ");
 
-						if (userBalanceArr.length == 2) {
+							// Identifies user balance and overDraft
+							if (userBalanceArr.length == 2) {
 
-							int userBalance = Integer.parseInt(userBalanceArr[0]);
-							int userOverdraft = Integer.parseInt(userBalanceArr[1]);
-							
-							userAccount.setBalance(userBalance);
-							userAccount.setOverDraft(userOverdraft);
-							
-							while(sc.hasNextLine()) {
-								String userSessionline = sc.nextLine();
-								String[] userInteractionArr = userSessionline.split(" ");
-								
-								if(userInteractionArr[0].equals("B")) {
-									bal.balance(atmMachine, userAccount);									
+								int userBalance = Integer.parseInt(userBalanceArr[0]);
+								int userOverdraft = Integer.parseInt(userBalanceArr[1]);
+
+								userAccount.setBalance(userBalance);
+								userAccount.setOverDraft(userOverdraft);
+
+								// need to add condition for blank line
+								// also stepping into userInteraction with the atm(balance , withdrawal)
+								while (sc.hasNextLine()) { // need to add condition for an empty line
+									String userSessionline = sc.nextLine();
+									String[] userInteractionArr = userSessionline.split(" ");
+									if (userInteractionArr[0].equals("B")) {
+										bal.balance(atmMachine, userAccount);
+									} else if (userInteractionArr[0].equals("W")) {
+										int withDrawalAmount = Integer.parseInt(userInteractionArr[1]);
+										// check if atm has enough money to withdraw cash
+										// user cant withdraw more funds then they actually have
+										if (atmMachine.getBalance() != 0
+												&& atmMachine.getBalance() >= withDrawalAmount) {
+											if (withDrawalAmount <= userBalance) {
+												int newUserBalance = userAccount.getBalance() - withDrawalAmount;
+												int newAtmBalance = atmMachine.getBalance() - withDrawalAmount;
+												userAccount.setBalance(newUserBalance);
+												atmMachine.setBalance(newAtmBalance);
+
+												System.out.println(userAccount.getBalance());
+												System.out.println(atmMachine.getBalance());
+											} else {
+												System.out.println(AtmError.FUNDS_ERR.getEnumDes());
+											}
+										} else {
+											System.out.println(AtmError.ATM_ERR.getEnumDes());
+										}
+									}
 								}
-								else if(userInteractionArr[0].equals("W")) {
-									int withDrawalAmount = Integer.parseInt(userInteractionArr[1]);
-									
-									int newUserBalance = userAccount.getBalance() - withDrawalAmount;
-									int newAtmBalance = atmMachine.getBalance() - withDrawalAmount;
-									userAccount.setBalance(newUserBalance);
-									atmMachine.setBalance(newAtmBalance);
-									
-									System.out.println(userAccount.getBalance());
-									System.out.println(atmMachine.getBalance());																		
-								}
+
 							}
+						} else {
+							System.out.println(AtmError.ACCOUNT_ERR.getEnumDes());
 						}
 					}
 				}
@@ -98,7 +117,6 @@ public class ATM {
 		}
 	}
 }
-
 
 /*
  * if (firstLine.matches(pattern1)) { int balance = Integer.parseInt(firstLine);
@@ -115,9 +133,8 @@ public class ATM {
  * userAccount.setBalance(userBalance);
  * 
  * details.accountDetails(line, balance, userBalance, balButton, withdrawal); //
- * possibly 3  arguements } else if (line.matches(pattern3)) {
+ * possibly 3 arguements } else if (line.matches(pattern3)) {
  * overdraft.draft(line, balance); // } // else if(line.matches(pattern4)) { //
  * bal.balance(line, balance); } // else if(line.matches(pattern5)) { //
  * w.withDrawal(line, balance); // } } }
  */
-
