f2fb0b444c08bd8bc48458eaf5668d90216455d6
aaron long legs <longaaron331@gmail.com>
2020-09-19 19:36:07 +0100

    test cases

--- src/test/java/atmEngine/atmTest.java
+++ src/test/java/atmEngine/atmTest.java
@@ -4,11 +4,53 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class atmTest {
+import com.atm.datamodel.AtmMachine;
+import com.atm.datamodel.UserAccount;
+import com.atm.service.atmServiceImpl;
+import com.atm.service.atmUserInteractionImpl;
+
+public class atmTest{
+	
+	AtmMachine atmMachine = new AtmMachine();
+	UserAccount userAccount = new UserAccount();
 
 	@Test
-	public void test() {
+	public void testIfAccountNumberIsNotNull() {
+		String accountNumber = userAccount.getAccountNumber();
 		
+		assertNotNull(accountNumber, "account Number is null");		
+		//assertNull(accountNumber, "account number is not null");
+	}
+	
+	@Test
+	public void testIfLengthIsCorrect() {
+		String accountNumber = userAccount.getAccountNumber();		
+		assertEquals(8, accountNumber.length());
+	}
+	
+	@Test
+	public void testIfPinNumberIsNotNull() {
+		String checkPin = userAccount.getAccountPin();
+		assertNotNull(checkPin, "pin entered is null");
+	}
+	
+	@Test
+	public void testIfPinIsEqual() {
+		String userEnteredPin = userAccount.getAccountPin();
+		
+		//assertEquals("need to figure how to get array from other class maybe", userEnteredPin);
+	}
+	
+	@Test
+	public void checkIfUserBalanceIsZero() {
+		int userBalance = userAccount.getBalance();	
+		assertNotNull(userBalance);
+	}
+	
+	@Test
+	public void checkIfAtmBalanceIsZero() {
+		int atmBalance = atmMachine.getBalance();	
+		assertNotNull(atmBalance);
 	}
 
 }
